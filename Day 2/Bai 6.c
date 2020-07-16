5b67aa3457a7e05c728c45f4e6fa59e6a1255139
phuchptty <phuc.px.371@gmail.com>
2020-07-16 20:08:11 +0700

    update b6 + b7

--- Day 2/Bai 6.c
+++ Day 2/Bai 6.c
@@ -7,57 +7,61 @@ int main(){
     printf("Nhap Ngay m (dd/mm/yyyy): ");
     scanf("%d/%d/%d",&d,&m,&y);
 
-    d = d + 1;
-
-    if (m == 2 && y % 4 != 0){
-        if (d == 29){
-            if (m + 1 > 12 ){
-                m = m +1 -12;
-                y = y +1;
-            }else{
-                m = m +1;
-            }
-        }        
-
-    }else if (m == 2 && y % 4 == 0){
-        if (d > 29){
-            d = 1;
-            m = 3;
-        }
+    if (d > 31 || m > 12){
+        printf("Ngay Thang Khong Hop Le");
     }else{
-        int a;
-        switch (m){
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                a = 31;
-                break;
-            
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                a = 30;
-                break;
-        }
+        d = d + 1;
 
-        if (d > a){
-            d = 1;
-            
-            if (m + 1 > 12 ){
-                m = 1;
-                y = y +1;
-            }else{
-                m = m +1;
+        if (m == 2 && y % 4 != 0){
+            if (d == 29){
+                if (m + 1 > 12 ){
+                    m = m +1 -12;
+                    y = y +1;
+                }else{
+                    m = m +1;
+                }
+            }        
+
+        }else if (m == 2 && y % 4 == 0){
+            if (d > 29){
+                d = 1;
+                m = 3;
+            }
+        }else{
+            int a;
+            switch (m){
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    a = 31;
+                    break;
+                
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    a = 30;
+                    break;
+            }
+
+            if (d > a){
+                d = 1;
+                
+                if (m + 1 > 12 ){
+                    m = 1;
+                    y = y +1;
+                }else{
+                    m = m +1;
+                }
             }
         }
+
+        printf("%d/%d/%d",d,m,y);
+
+        getch();
     }
-
-    printf("%d/%d/%d",d,m,y);
-
-    getch();
 }
---
+++ Day 2/Bai 7b.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int a,b,min,ucln;
+
+    printf("Nhap phan so (a/b): ");
+    scanf("%d/%d",&a,&b);
+
+    if (a == 0 || b == 0){
+        printf("Khong ton tai phan so");
+    }else if (a == b){
+        printf("Phan so toi gian = %d", a /b);
+    }else{
+
+        if (a < b){
+            min = a;
+        }else{
+            min = b;
+        }
+        
+        for (int i = 1; i <= min; i++) {
+            if (a%i == 0 && b%i == 0){
+                ucln = i;
+            }
+        }
+
+        if (ucln == 1){
+            printf("Phan so khong toi gian duoc nua");
+        }else{
+            a = a / ucln;
+            b = b / ucln;
+
+            printf("Uoc chung lon nhat la: %d \n", ucln);
+            printf("Phan so toi gian la: %d/%d",a,b);
+        }
+    }
+
+    getch();
+}
