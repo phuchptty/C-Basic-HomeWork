034d6d9f5360435cffc5a416e734a2922c92b037
phuchptty <phuc.px.371@gmail.com>
2020-07-17 08:50:06 +0700

    update complete bai 8

--- Day 2/Bai 8.c
+++ Day 2/Bai 8.c
@@ -31,11 +31,55 @@ int main() {
                 printf("*");
             }
 
-            for (int a = 1; a <= sp; a++) {
-                printf(" ");
+            printf("\n");
+        }
+
+        printf("\n\n");
+
+        for (int i = 1; i <= h; i++) {
+
+            if (i == 1) {
+                int sp = totalCharInLayer - i;
+                int space = sp / 2;
+
+                for (int a = 1; a <= sp; a++) {
+                    printf(" ");
+                }
+
+                int star = 1 + (i - 1) * 2;
+
+                for (int c = 1; c <= star; c++) {
+                    printf("*");
+                }
+
+            } else {
+
+                for (int a = 1; a <= totalCharInLayer - i; a++) {
+                    printf(" ");
+                }
+
+                int charInLayer = 1 + (i - 1) * 2;
+
+                for (int a = 1; a <= charInLayer; a++) {
+
+                    if (i == h) {
+
+                        printf("*");
+
+                    } else {
+                        if (a == 1 || a == charInLayer) {
+                            printf("*");
+                        } else {
+                            printf(" ");
+                        }
+                    }
+                }
             }
 
             printf("\n");
         }
     }
+
+    getch();
+    return 0;
 }
